9d4e8288456adc92df2b738ffb08db5acd9f115b
Sacimia <88517353+Sacimia@users.noreply.github.com>
2023-08-22 21:13:48 -0300

    Update oi.py

--- oi.py
+++ oi.py
@@ -1,8 +1,7 @@
-print("helloworld")
 n = 1000
 for i in range(1,n+1):
   print (i, " oi")
       if(i%2)==0:
-        print(i, "eh par")
+          print(i, "eh par")
       else:
-         print(i, "eh impar")
+          print(i, "eh impar")
