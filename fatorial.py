ab5aaca0c5598012291c36f607dd1a1ebba2795b
Sacimia <88517353+Sacimia@users.noreply.github.com>
2023-09-11 20:26:47 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,10 @@
+##Programa de imprimir várias vezes váriaveis
+
+
+import math
+from math import factorial
+number= 10000
+factorial = 1
+for i in range(1, number + 1):
+    factorial = (number - 1)
+    print(factorial)
