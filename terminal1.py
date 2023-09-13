6760a5e98875b33ad56e984c906afdbf4adfd361
Sacimia <88517353+Sacimia@users.noreply.github.com>
2023-09-12 22:11:10 -0300

    Create terminal1.py

---
+++ terminal1.py
@@ -0,0 +1,22 @@
+##Usando o terminal do python ou de qualquer IDLE que tenha python, a  gente utiliza esses atribuitos 
+
+
+nome = "Isabela de Braganca e Orleans"
+nome[0]
+nome[1]
+nome[7]
+nome[0:5]
+nome[-1]
+nome[:5]
+
+#Contador de "váriaveis"
+nome.count('a')
+
+##Ache
+nome.find('a')
+
+#Maiuscula
+nome.upper('a')
+nome.lstrip('a')
+nome.replace('a', 'A')
+nome.isdigit()
