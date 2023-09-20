c3667207dbbb83ac7e15e20e53172420a272572b
Sacimia <88517353+Sacimia@users.noreply.github.com>
2023-09-19 21:17:29 -0300

    Create fibonacci2.py

---
+++ fibonacci2.py
@@ -0,0 +1,17 @@
+print("-"*50)
+n = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
+
+
+if (n==1) or (n==2):
+    print("1")
+else:
+    count=3
+    while count <= n:
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        count += 1
+    print(F"O VALOR DE ENESIMO TERMO DA FUNÇAO DE FIBONACCI É {termo}")
+print("-"*50)
